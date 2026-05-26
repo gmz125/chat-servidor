e6f72bf630a2ec7adc99d5d211ee2ff4fc996cc6
GMZ125 <222t0239@itsm.edu.mx>
2026-05-25 20:33:21 -0600

    conector mysql agregado y db.js configurado

--- db.js
+++ db.js
@@ -1,45 +1,15 @@
-// db.js
-// Configuración de conexión a PostgreSQL en Railway
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config();
 
+const pool = mysql.createPool(process.env.MYSQL_URL); // Creamos el pool directamente con la URL pública de Railway
 
-import pkg from 'pg';
-const { Pool } = pkg;
-
-
-const pool = new Pool({
-  user: 'postgres',
-  host: 'zephyr.proxy.rlwy.net',
-  database: 'railway',
-  password: 'YVakmsMCghvogFFjrpHNzoJOhLlBbfZh',
-  port: 59829,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-
-// Código de prueba de conexión
-
-// Prueba de conexión: esto se ejecuta siempre que corras node db.js
-pool.query('SELECT NOW()', (err, res) => {
-  if (err) {
-    console.error('Error de conexión:', err);
-  } else {
-    console.log('Conexión exitosa:', res.rows);
-  }
-  pool.end();
-});
+try {
+  const connection = await pool.getConnection();
+  console.log('¡Conexión exitosa a la base de datos MySQL en Railway!');
+  connection.release();
+} catch (error) {
+  console.error('Error al conectar a la base de datos:', error.message);
+}
 
 export default pool;
-
-// Puedes probar la conexión ejecutando este archivo con:
-// node db.js
-// Y agregando este código al final:
-// pool.query('SELECT NOW()', (err, res) => {
-//   if (err) {
-//     console.error('Error de conexión:', err);
-//   } else {
-//     console.log('Conexión exitosa:', res.rows);
-//   }
-//   pool.end();
-// });
